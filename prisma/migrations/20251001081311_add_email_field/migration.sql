df89c1da084b125cfc0542986a97b5d17846677f
jingu0318 <18dlwlsrn@gmail.com>
2025-10-01 17:19:10 +0900

    sql 수정 마이그레이션 추가

---
+++ prisma/migrations/20251001081311_add_email_field/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."User";
+
+-- CreateTable
+CREATE TABLE "public"."user_tb" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_tb_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_tb_clerkId_key" ON "public"."user_tb"("clerkId");
