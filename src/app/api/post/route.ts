dbe01cfbbba980809fdedd7c1d91e558ba22fdc5
jingu0318 <18dlwlsrn@gmail.com>
2025-10-12 17:00:45 +0900

    라우터 작성자 부분추가

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 
-export let posts: { id: string; title: string; content: string }[] = [];
+export let posts: { id: string; title: string; content: string; author: string }[] = [];
 
 export async function GET() {
   return NextResponse.json(posts);
 }
 
 export async function POST(req: Request) {
-  const { title, content } = await req.json();
+  const { title, content, author } = await req.json();
   const id = Date.now().toString();
-  posts.unshift({ id, title, content });
-  return NextResponse.json({ id, title, content });
+  posts.unshift({ id, title, content, author });
+  return NextResponse.json({ id, title, content, author });
 }
