7f00971b3547dcb53904510a4a18a67ba037258a
jingu0318 <18dlwlsrn@gmail.com>
2025-10-12 17:02:22 +0900

    글추가 부분 component로 분리 & auth 인증 부분(server client 구분)

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,45 +1,13 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import NewPostForm from "@/components/posts/NewPostForm";
 
-export default function NewPostPage() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const router = useRouter();
+export default async function NewPostPage() {
+  const { userId } = await auth();              
 
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault(); //SPA 처럼 동작하기 위해 페이지 새로고침 막기위해 호출
-    await fetch("/api/post", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ title, content }),
-    });
-    router.push("/posts");
+  if (!userId) {
+    redirect("/sign-in");
   }
 
-  return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div>
-        <label className="block">제목</label>
-        <input
-          className="border px-2 py-1 w-full"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label className="block">내용</label>
-        <textarea
-          className="border px-2 py-1 w-full"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          required
-        />
-      </div>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded" type="submit">
-        추가
-      </button>
-    </form>
-  );
+  return <NewPostForm />;
 }
---
+++ src/components/posts/NewPostForm.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function NewPostPage() {
+  const { user, isSignedIn } = useUser();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const router = useRouter();
+
+  if (!isSignedIn) {
+    router.push("/sign-in");
+    return null;
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    await fetch("/api/post", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title,
+        content,
+        author: user?.firstName + (user?.lastName ? " " + user.lastName : ""),
+      }),
+    });
+    router.push("/posts");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <div>
+        <label className="block">제목</label>
+        <input
+          className="border px-2 py-1 w-full"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label className="block">내용</label>
+        <textarea
+          className="border px-2 py-1 w-full"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required
+        />
+      </div>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded" type="submit">
+        추가
+      </button>
+    </form>
+  );
+}
